becea5d28bc0f219378f9cde21b8706d98109ab9
tinashub <tina4purpose@gmail.com>
2019-03-01 08:52:08 +0100

    Implemented all the functionalities
    Implemented: random speed for enemies, movement limits across the board,
    collisions, winning state, change player avatar and reset positions when
    player wins.
    index.html changed to amend the title of the page.
    engine.js changed to load more images.
    
    Readme file completed

--- README.md
+++ README.md
@@ -2,19 +2,43 @@
 
 ## Table of Contents
 
-- [Instructions](#instructions)
-- [Contributing](#contributing)
+* [Description](#description)
+* [Instructions](#instructions)
+* [Dependencies](#dependencies)
+* [Contributing](#contributing)
+
+## Description
+
+Classic Arcade Game. The Player is to reach the water on the other side of their initial position on the grass, avoiding collision with Enemies.
+
+Enemies run across the board on random speeds. Upon collision with one, Player is returned to initial point, starting again.
+
+Player wins when they reach the water and their avatar is "upgraded" to a new image, then the game starts again.
 
 ## Instructions
 
-Use this [rubric](https://review.udacity.com/#!/rubrics/15/view) for self-checking your submission.
+Please download the complete game including index.html file and folders css, images and js.
 
-Make sure the functions you write are **object-oriented** - either class functions (like `Player` and `Enemy`) or class prototype functions such as `Enemy.prototype.checkCollisions`. Also make sure that the keyword `this` is used appropriately within your class and class prototype functions to refer to the object the function is called upon.
+To start the game, double-click index.html.
 
-Your **README.md** file should be updated with instructions on both how to 1. Run and 2. Play your arcade game.
+You are the Player. Your goal is to reach the water, without colliding into any one of the Enemies (bugs).
 
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+Use left, right, up and down keys to move. The Enemies move across the paved block portion of the game board.
+
+If you collide with an Enemy, you are moved back to the starting point on the grass.
+
+You win the game when you reach the water (i.e., the top of the game board). Your avatar is then "upgraded", and the game starts again.
+
+When you get the "top" avatar (with the crown), you know that you are doing great! You then go back to the initial avatar and start all over again from scratch.
+
+Good luck!
+
+## Dependencies
+
+Along with run file index.html, the app uses style.css file located in css folder, app.js, engine.js and resources.js files in js folder and all the images loaded by engine.js from images folder.
 
 ## Contributing
 
-This repository is the starter code for _all_ Udacity students. Therefore, we most likely will not accept pull requests.
+Starter code provided by Udacity.
+
+Final version developed by Valentina Perfilov.
--- js/app.js
+++ js/app.js
@@ -1,22 +1,30 @@
+// store images to change each time player wins
+const playerImg = ['images/char-boy.png','images/char-pink-girl.png','images/char-cat-girl.png','images/char-horn-girl.png','images/char-princess-girl.png'];
+
+// store row height and column width of the game board as const to manage measures easily
+const rowH = 83;
+const colW = 101;
+
 // Enemies our player must avoid
 let Enemy = function(y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-	this.x = 0;
+    // Variables applied to each of Enemy instances: x and y ini coord., random speed 10-800 px per sec, and image
+	this.x = -colW*3;
 	this.y = y;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+	this.randSpeed = Math.random()*790+10;
+	this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
+// Update the enemy's position, required method for game. Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-	this.x += dt;
-	//this.y = 0;
+    // Multiplying randSpeed by dt gives the progress of enemy along x coord. each time the browser draws the scene
+	this.x += this.randSpeed*dt;
+	if (this.x > colW*10) {
+		this.x = -colW*10;
+	}
+	// Check if collision - then reset
+	if ((this.y == player.y)&&(this.x+colW >= player.x+colW/4)&&(this.x <= player.x+colW*3/4)) {
+		resetPositions(200);
+	}
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,54 +32,72 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player class with update(), render() and handleInput() methods
 function Player() {
-	this.x = 300;
-	this.y = 400;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/char-boy.png';
-	this.update = function(dt) {
-		// You should multiply any movement by the dt parameter
-		// which will ensure the game runs at the same speed for
-		// all computers.
-		//this.x += dt;
-		//this.y = 0;
+	this.x = colW*2;
+ 	this.y = rowH*5-20;
+	this.numImg = 0;
+    this.sprite = playerImg[0];
+	this.update = function() {
+		// Note: the update functionality is implemented in handleInput
 	};
 	this.render = function() {
 		ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 	};
+	// Process the key returned by keyPressed event listener
 	this.handleInput = function(keyPressed) {
 		switch (keyPressed) {
-		  case 'left':
-			this.x -= 100;
-			break;
-		  case 'up':
-			this.y -= 82;
-			break;
-		  case 'right':
-			this.x += 100;
-			break;
-		  case 'down':
-			this.y += 82;
+			case 'left':
+				if (this.x >= colW) {
+					this.x -= colW;
+				}
+				break;
+			case 'right':
+				if (this.x <= colW*3) {
+					this.x += colW;
+				}
+				break;
+			case 'down':
+				if (this.y <= rowH*4-20) {
+					this.y += rowH;
+				}
+				break;
+			case 'up':
+				if (this.y >= rowH-20) {
+					this.y -= rowH;
+					// Check if player won - then "upgrade" player's character image and reset
+					if (this.y < rowH-20) {
+						this.numImg++;
+						if (this.numImg == 5) {
+							this.numImg = 0;
+						}
+						this.sprite = playerImg[this.numImg];
+						resetPositions(700);
+					}
+				}
 		}
 	};
-};
-
-// Now instantiate your objects.
+}
 
 // Place all enemy objects in an array called allEnemies
-
-let allEnemies = [new Enemy(70), new Enemy(150), new Enemy(230)];
+let allEnemies = [new Enemy(rowH-20), new Enemy(rowH*2-20), new Enemy(rowH*3-20), new Enemy(rowH-20), new Enemy(rowH*2-20), new Enemy(rowH*3-20)];
 
 // Place the player object in a variable called player
-
 let player = new Player();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Reset all characters positions
+function resetPositions(delayMsec) {
+	setTimeout(function (delayMsec) {
+		allEnemies.forEach(function(enemy) {
+			enemy.x = -colW*3;
+			enemy.randSpeed = Math.random()*700+100;
+		});
+		player.x = colW*2;
+		player.y = rowH*5-20;
+	}, delayMsec);
+}
+
+// This listens for key pressed and sends the key to Player.handleInput() method
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -79,6 +105,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
